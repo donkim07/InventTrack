d7099b942140bc5cad6bf083611fcc61d83f8267
Donald <donestomih@gmail.com>
2025-05-03 03:01:28 +0300

    Create companies table with detailed fields for company information

--- database/migrations/2025_05_02_233527_create_companies_table.php
+++ database/migrations/2025_05_02_233527_create_companies_table.php
@@ -12,8 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('companies', function (Blueprint $table) {
-            $table->id();
+            $table->id('company_id');
+            $table->integer('owner_user_id')->nullable();
+            $table->string('name')->unique();
+            $table->string('subdomain')->unique();
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('country')->nullable();
+            $table->string('address')->nullable();
+            $table->string('website')->nullable();
+            $table->string('logo_url')->nullable();
+            $table->string('description')->nullable();
+            $table->date('license_expiry_date')->nullable();
+            $table->string('payment_status')->nullable();
+            $table->string('status')->default('active');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
