b63fe8961589db50a055f2f25987a1c178322d27
Donald <donestomih@gmail.com>
2025-05-05 23:57:06 +0300

    Refactor admin route group to include namespace, middleware, and alias configuration

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -6,7 +6,9 @@ Admin::routes();
 
 Route::group([
     'prefix'        => config('admin.route.prefix'),
-
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
