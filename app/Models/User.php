d377f1b5b3541d86a2c65fb53ad410b46047c362
Donald <donestomih@gmail.com>
2025-05-06 23:58:03 +0300

    Remove unnecessary casts from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,9 +40,6 @@ class User extends Authenticatable
      */
     protected function casts(): array
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        
     }
 }
