3b08abc73d134d7cac353b1a08803d2f7a93f44c
Donald <donestomih@gmail.com>
2025-05-13 21:37:51 +0300

    Add title property and method to AuthController for improved title management

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -6,5 +6,17 @@ use Encore\Admin\Controllers\AuthController as BaseAuthController;
 
 class AuthController extends BaseAuthController
 {
+    /**
+     * @var string
+     */
+    protected $title = 'Auth';
+
+    /**
+     * @return string
+     */
+    protected function title(): string
+    {
+        return $this->title;
+    }
 
 }
