6013b3300d3ddb09aeb11d7b4a26ed1b28f810bc
Donald <donestomih@gmail.com>
2025-05-13 21:41:24 +0300

    Remove domain configuration from admin route group for simplification

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -9,8 +9,6 @@ Route::group([
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
     'as'            => config('admin.route.prefix') . '.',
-    'domain'        => config('admin.route.domain'),
-    
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('home');
