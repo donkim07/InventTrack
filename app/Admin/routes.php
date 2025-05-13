5d8fbdf8b3fba03b6c0866416b47465f48a63ad5
Donald <donestomih@gmail.com>
2025-05-13 21:40:09 +0300

    Add resource routes for various controllers to enhance routing structure

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,5 +13,24 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('home');
     $router->resource('companies', CompanyController::class);
+    $router->resource('users', UserController::class);
+    $router->resource('auth', AuthController::class);
+    $router->resource('roles', RoleController::class);
+    $router->resource('permissions', PermissionController::class);
+    $router->resource('admins', AdminController::class);
+    $router->resource('settings', SettingController::class);
+    $router->resource('logs', LogController::class);
+    $router->resource('activity-logs', ActivityLogController::class);
+    $router->resource('notifications', NotificationController::class);
+    $router->resource('audit-logs', AuditLogController::class);
+    $router->resource('backups', BackupController::class);
+    $router->resource('translations', TranslationController::class);
+    $router->resource('menus', MenuController::class);
+    $router->resource('widgets', WidgetController::class);
+    $router->resource('themes', ThemeController::class);
+    $router->resource('languages', LanguageController::class);
+    $router->resource('files', FileController::class);
+    $router->resource('media', MediaController::class);
+    
 
 });
